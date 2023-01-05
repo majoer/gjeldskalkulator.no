09d06c3dda23ade7683c49b1de13b1c9048af5de
Jørgensen, Mats <matsjoer@gmail.com>
2023-01-06 00:59:22 +0100

    Format landing page

--- pages/index.tsx
+++ pages/index.tsx
@@ -22,20 +22,27 @@ export default function Home() {
       </Head>
 
       <main className="h-5/6 text-center">
-        <Container className="h-full pt-40">
+        <Paper className="h-full w-full pt-40">
           <Paper className="h-1/3 flex flex-col justify-center">
-            <Button LinkComponent={Link} href="calculator">
-              Lets get started!
-            </Button>
+            <div>
+              <Button LinkComponent={Link} href="calculator">
+                Lets get started!
+              </Button>
+            </div>
           </Paper>
-        </Container>
+        </Paper>
       </main>
 
       <footer className="h-1/6">
         <Paper className="h-full p-4 flex flex-col-reverse" elevation={6}>
-          <a href="https://www.flaticon.com/free-icons/debt" title="Page icon">
-            Page icon created by Freepik - Flaticon
-          </a>
+          <div className="flex flex-row justify-between">
+            <a
+              href="https://www.flaticon.com/free-icons/debt"
+              title="Page icon"
+            >
+              Page icon created by Freepik - Flaticon
+            </a>
+          </div>
         </Paper>
       </footer>
 
