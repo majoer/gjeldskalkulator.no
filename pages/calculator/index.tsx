da39b8fd0ea6659e75875c55052c5d16d572da20
Jørgensen, Mats <matsjoer@gmail.com>
2023-01-06 00:47:43 +0100

    Fix some styling inconsistencies

--- pages/calculator/index.tsx
+++ pages/calculator/index.tsx
@@ -4,11 +4,11 @@ import AppUserInputComponent from "../../components/app-user-input-component";
 
 export default function CalculatorPage() {
   return (
-    <Paper className="h-full-header">
-      <div className="w-full xl:w-1/2 mb-4 xl:mb-0 bg-slate-100">
+    <Paper className="min-h-screen h-100%">
+      <div className="w-full xl:w-1/2 mb-4 pt-16 xl:mb-0 bg-slate-100">
         <AppUserInputComponent />
       </div>
-      <div className="w-full xl:w-1/2 static top-64px xl:fixed xl:bottom-0 xl:right-0">
+      <div className="w-full xl:w-1/2 static top-16 xl:fixed xl:bottom-0 xl:right-0">
         <AppDebtInsightComponent />
       </div>
     </Paper>
