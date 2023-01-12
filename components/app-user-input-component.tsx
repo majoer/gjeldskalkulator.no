207dc0c85a882f1e983aed08d14e5f0a91038136
Jørgensen, Mats <matsjoer@gmail.com>
2023-01-12 01:02:12 +0100

    Add scroll anchor to bottom

--- components/app-user-input-component.tsx
+++ components/app-user-input-component.tsx
@@ -89,6 +89,7 @@ export default function AppUserInputComponent() {
           <AppDebtsComponent />
         </AccordionDetails>
       </Accordion>
+      <div className="h-2"></div>
     </div>
   );
 }
--- pages/kalkulator/index.tsx
+++ pages/kalkulator/index.tsx
@@ -6,7 +6,7 @@ import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 export default function CalculatorPage() {
   return (
     <Paper className="min-h-screen h-100%">
-      <div className="w-full xl:w-1/2 mb-4 pt-16 xl:mb-0 bg-slate-100">
+      <div className="w-full xl:min-h-screen xl:w-1/2 mb-4 pt-16 xl:mb-0 bg-slate-100">
         <AppUserInputComponent />
       </div>
       <div className="w-full xl:w-1/2 static top-16 xl:fixed xl:bottom-0 xl:right-0">
