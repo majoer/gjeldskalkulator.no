fb77556f6abb7eaadda08a9a9124a4e434b8f426
Jørgensen, Mats <matsjoer@gmail.com>
2023-01-14 23:32:33 +0100

    Add air below guide card

--- components/guide/app-guide-card-component.tsx
+++ components/guide/app-guide-card-component.tsx
@@ -29,7 +29,7 @@ export default function AppGuideCardComponent({
 
   return (
     <div className="w-full text-center">
-      <div className="w-full h-3/6 xl:mt-32">
+      <div className="w-full h-3/6 xl:mt-32 mb-10">
         <Card
           elevation={10}
           className="relative lg:w-2/3 xl:w-5/12 lg:m-auto p-6"
