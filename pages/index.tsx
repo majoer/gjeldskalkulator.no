6b0adec750673b05b3e99821e3c7a9ef007968bc
Jørgensen, Mats <matsjoer@gmail.com>
2023-01-30 21:44:00 +0100

    Fix language for front-page

--- pages/index.tsx
+++ pages/index.tsx
@@ -58,10 +58,10 @@ export default function LandingPage() {
   );
 }
 
-export async function getStaticProps({ locale }) {
+export async function getServerSideProps({ locale }) {
   return {
     props: {
-      ...(await serverSideTranslations(locale, ["common", "contact-us"])),
+      ...(await serverSideTranslations(locale, ["common", "landing", "guide"])),
     },
   };
 }
