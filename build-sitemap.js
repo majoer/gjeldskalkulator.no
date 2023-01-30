89d9470105330f4ab615e73a912a99677b03e220
Jørgensen, Mats <matsjoer@gmail.com>
2023-01-30 01:06:15 +0100

    Add sitemap for english language

--- build-sitemap.js
+++ build-sitemap.js
@@ -2,13 +2,13 @@ const { SitemapStream, streamToPromise } = require('sitemap')
 const { Readable } = require('stream')
 const fs = require('fs')
 
-
+const extraLanguages = ['en-US']
 const links = [
   { url: '/', changefreq: 'daily', priority: 1 },
   { url: '/kalkulator/', changefreq: 'daily', priority: 1 },
   { url: '/guide/bil/', changefreq: 'daily', priority: 0.7 },
   { url: '/guide/bolig/', changefreq: 'daily', priority: 0.7 },
-  { url: '/guide/budsjett/', changefreq: 'daily', priority: 0.7, },
+  { url: '/guide/budsjett/', changefreq: 'daily', priority: 0.7 },
   { url: '/guide/kreditt/', changefreq: 'daily', priority: 0.7 },
   { url: '/guide/refinansiering/', changefreq: 'daily', priority: 0.7 },
   { url: '/kontakt-oss/', changefreq: 'daily', priority: 0 },
@@ -19,11 +19,23 @@ const links = [
   // { url: '/guide/dnb/importer-budsjett', changefreq: 'daily', priority: 1 },
 ]
 
+const sitemapLocs = links
+  .map(link => ({ ...link, lastmod: new Date() }))
+  .map(link => ({
+    url: link.url,
+    links: [
+      { ...link, lang: 'nb' },
+      ...extraLanguages.map(lang => ({ ...link, url: `${lang}${link.url}`, lang }))
+    ]
+  }))
+
+console.log(JSON.stringify(sitemapLocs, null, 2))
+
 
 // Create a stream to write to
 const stream = new SitemapStream({ hostname: 'https://www.gjeldskalkulator.no' })
 
 // Return a promise that resolves with your XML string
-streamToPromise(Readable.from(links.map(page => ({ ...page, lastmod: new Date() }))).pipe(stream)).then((data) =>
+streamToPromise(Readable.from(sitemapLocs).pipe(stream)).then((data) =>
   fs.writeFileSync("public/sitemap.xml", data)
 )
