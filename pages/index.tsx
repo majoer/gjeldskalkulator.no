d6bafedb7a6602e2bf6f7132e1df0e59ed58e27e
Jørgensen, Mats <matsjoer@gmail.com>
2023-01-31 00:11:22 +0100

    Padding bottom index

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,7 +16,7 @@ export default function LandingPage() {
   return (
     <AppPageLayoutComponent title={t("landing:title")} description={t("contact-us:description")}>
       {/* <title>Lånekalkulator, budsjetthjelp og tips til refinansiering av kreditt</title> */}
-      <div className="inline-grid gap-3 grid-cols-1 md:grid-cols-2 xl:grid-cols-3">
+      <div className="inline-grid gap-3 grid-cols-1 md:grid-cols-2 xl:grid-cols-3 pb-4">
         <AppClickableCardComponent href="/guide/bil">
           <CardHeader title={t("guide:car.title")} />
           <CardMedia>
